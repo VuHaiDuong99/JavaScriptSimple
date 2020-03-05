809133f029650c185a379df70e5d0d8920bb8474
=VuHaiDuong99 <=>
2020-03-05 21:51:27 +0700

    lesson confirm and promit

--- bai1.js
+++ bai1.js
@@ -1,1 +1,10 @@
-alert("Vu Hai Duong")
+/*var result = confirm("Ban co chac chan muon xoa khong ? ");
+if(result == true ){
+    alert("Ban Xoa Thanh cong");
+}
+else{
+    alert("Ban Xoa Khong Thanh Cong");
+}*/
+var result = prompt("Please , tell me your name")// prompt("Please , tell me your name","No name"), 
+//, khi bam enter no se hien thi ra 'No Name'
+alert(result);
